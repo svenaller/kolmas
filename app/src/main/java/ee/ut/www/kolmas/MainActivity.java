5f82a72a36e80c6a2ca526186817c3794fb0d51e
svenaller <allersven@gmail.com>
2015-03-09 11:50:49 +0200

    CameraActivity

--- app/src/main/java/ee/ut/www/kolmas/MainActivity.java
+++ app/src/main/java/ee/ut/www/kolmas/MainActivity.java
@@ -1,32 +1,58 @@
 package ee.ut.www.kolmas;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    Button btnTakePhoto;
+    ImageView takenPhoto;
+    private static final int CAM_REQUEST = 1313;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        btnTakePhoto = (Button) findViewById(R.id.btnTakePhoto);
+        takenPhoto = (ImageView) findViewById(R.id.takenPhoto);
+        btnTakePhoto.setOnClickListener(new btnTakePhotoClicker());
+    }
 
-
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CAM_REQUEST) {
+            Bitmap thumbnail = (Bitmap) data.getExtras().get("data");
+            takenPhoto.setImageBitmap(thumbnail);
+        }
     }
 
     public void pressbutton(View v) {
         TextView text = (TextView) findViewById(R.id.text);
-
         if (text.getText() == "One") {
             text.setText("Two");
         } else {
             text.setText("One");
+        }
+    }
+
+    class btnTakePhotoClicker implements Button.OnClickListener
+    {
+        public void onClick(View v) {
+            Intent cameraintent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            startActivityForResult(cameraintent, CAM_REQUEST);
+
         }
     }
 
